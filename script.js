2c8e8a8eb60e51e77bf8281d1dc8418fcca6c0c0
Ndubuaku Chigozie Emmanuel <victoriousman711@gmail.com>
2024-07-04 09:19:46 +0100

    Added some features

--- script.js
+++ script.js
@@ -100,85 +100,103 @@ alert(message);
 /* STOP WATCH FUNCTIONALITIES */
 
 // BUTTON VARIABLES
-const startStopBtn = document.querySelector(".startStopBtn");
-const resetBtn = document.querySelector(".resetBtn");
+// const startStopBtn = document.querySelector(".startStopBtn");
+// const resetBtn = document.querySelector(".resetBtn");
 
-// TIMER VARIABLES
+// // TIMER VARIABLES
 
-let seconds = 0;
-let minutes = 0;
-let hours = 0;
+// let seconds = 0;
+// let minutes = 0;
+// let hours = 0;
 
-// ADDING LEADING TIMER WITH A FUNCTION
+// // ADDING LEADING TIMER WITH A FUNCTION
 
-let leadingSeconds = 0;
-let leadingMinutes = 0;
-let leadingHours = 0;
+// let leadingSeconds = 0;
+// let leadingMinutes = 0;
+// let leadingHours = 0;
 
-// ADDING FUNCTIONALITY TO THE BUTTONS
-let timerInterval = null;
-let timerStatus = "stopped";
+// // ADDING FUNCTIONALITY TO THE BUTTONS
+// let timerInterval = null;
+// let timerStatus = "stopped";
 
-function stopWatch() {
-  seconds++;
-  if (seconds / 60 === 1) {
-    seconds = 0;
-    minutes++;
-  }
-  if (minutes / 60 === 1) {
-    minutes = 0;
-    hours++;
-  }
+// function stopWatch() {
+//   seconds++;
+//   if (seconds / 60 === 1) {
+//     seconds = 0;
+//     minutes++;
+//   }
+//   if (minutes / 60 === 1) {
+//     minutes = 0;
+//     hours++;
+//   }
 
-  if (seconds < 10) {
-    leadingSeconds = "0" + seconds.toString();
-  } else {
-    leadingSeconds = seconds;
-  }
-  if (minutes < 10) {
-    leadingMinutes = "0" + minutes.toString();
-  } else {
-    leadingMinutes = minutes;
-  }
-  if (hours < 10) {
-    leadingHours = "0" + hours.toString();
-  } else {
-    leadingHours = hours;
-  }
+//   if (seconds < 10) {
+//     leadingSeconds = "0" + seconds.toString();
+//   } else {
+//     leadingSeconds = seconds;
+//   }
+//   if (minutes < 10) {
+//     leadingMinutes = "0" + minutes.toString();
+//   } else {
+//     leadingMinutes = minutes;
+//   }
+//   if (hours < 10) {
+//     leadingHours = "0" + hours.toString();
+//   } else {
+//     leadingHours = hours;
+//   }
 
-  document.querySelector("#timer").innerHTML =
-    leadingHours + ":" + leadingMinutes + ":" + leadingSeconds;
-}
+//   document.querySelector("#timer").innerHTML =
+//     leadingHours + ":" + leadingMinutes + ":" + leadingSeconds;
+// }
 
-startStopBtn.addEventListener("click", function () {
-  if (timerStatus === "stopped") {
-    timerInterval = window.setInterval(stopWatch, 1000);
+// startStopBtn.addEventListener("click", function () {
+//   if (timerStatus === "stopped") {
+//     timerInterval = window.setInterval(stopWatch, 1000);
 
-    startStopBtn.innerHTML = `<img src="image/pause.png" alt="" class="button__img" id="pause" />`;
+//     startStopBtn.innerHTML = `<img src="image/pause.png" alt="" class="button__img" id="pause" />`;
 
-    timerStatus = "started";
-  } else {
-    window.clearInterval(timerInterval);
+//     timerStatus = "started";
+//   } else {
+//     window.clearInterval(timerInterval);
 
-    startStopBtn.innerHTML = `<img src="image/play.png" alt="" class="button__img" id="play" />`;
+//     startStopBtn.innerHTML = `<img src="image/play.png" alt="" class="button__img" id="play" />`;
 
-    timerStatus = "stopped";
-  }
+//     timerStatus = "stopped";
+//   }
+// });
+
+// resetBtn.addEventListener("click", function () {
+//   seconds = 0;
+//   minutes = 0;
+//   hours = 0;
+
+//   window.clearInterval(timerInterval);
+
+//   document.querySelector("#timer").innerHTML = "00:00:00";
+// });
+
+// // DOM PRACTICE
+
+// console.dir(document);
+// console.log(document.title);
+// console.log(document.body);
+// console.log(document.getElementById("fjfjj"));
+
+const burger = document.querySelector(".burger");
+const navLinks = document.querySelector(".nav-links");
+const links = document.querySelectorAll(".nav-links li");
+
+burger.addEventListener("click", () => {
+  navLinks.classList.toggle("nav-active");
+  links.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = "";
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${
+        index / 7 + 0.3
+      }s`;
+    }
+  });
+  burger.classList.toggle("toggle");
 });
-
-resetBtn.addEventListener("click", function () {
-  seconds = 0;
-  minutes = 0;
-  hours = 0;
-
-  window.clearInterval(timerInterval);
-
-  document.querySelector("#timer").innerHTML = "00:00:00";
-});
-
-// DOM PRACTICE
-
-console.dir(document);
-console.log(document.title);
-console.log(document.body);
-console.log(document.getElementById("fjfjj"));
